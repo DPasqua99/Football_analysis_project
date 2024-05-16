44f0c1489cb8b13031cdd44ad5ec96b931e2fb0a
Davide Pasquadibisceglie <pasquadibisceglie.davide99@gmail.com>
2024-05-16 17:53:08 +0200

    [+] Add referee tracker and number under player

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -80,7 +80,7 @@ class Tracker:
 
         return tracks
 
-    def draw_ellipse(self, frame, bbox, color, track_id):
+    def draw_ellipse(self, frame, bbox, color, track_id=None):
         y2 = int(bbox[3])
 
         x_center, _ = get_center_of_bbox(bbox)
@@ -97,6 +97,36 @@ class Tracker:
             thickness=2,
             lineType=cv2.LINE_4
         )
+
+        rectangle_width = 40
+        rectangle_high = 20
+        x1_rect = x_center - rectangle_width//2
+        x2_rect = x_center + rectangle_width//2
+        y1_rect = (y2 - rectangle_high//2) + 15
+        y2_rect = (y2 + rectangle_high//2) + 15
+
+        if track_id is not None:
+            cv2.rectangle(
+                frame,
+                (int(x1_rect),int(y1_rect)),
+                (int(x2_rect),int(y2_rect)),
+                color,
+                cv2.FILLED
+            )
+            x1_text = x1_rect + 12
+            if track_id > 99:
+                x1_text -= 10
+
+            cv2.putText(
+                frame,
+                f"{track_id}",
+                (int(x1_text), int(y1_rect+15)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.6,
+                (0,0,0),
+                2
+            )
+
         return frame
 
 
@@ -112,6 +142,10 @@ class Tracker:
             #Draw players
             for track_id, player in player_dict.items():
                 frame = self.draw_ellipse(frame, player["bbox"], (0,0,225), track_id)
+
+            #Draw referees
+            for _, referee in referee_dict.items():
+                frame = self.draw_ellipse(frame, referee["bbox"], (0,225,225), track_id)
 
             output_video_frames.append(frame)
 
